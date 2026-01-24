8780dca7c1a633f43dadfe66f397741aa658957e
WickedCode <WickedCode@mail.ru>
2026-01-25 01:58:16 +0300

    fix state toggle animations

--- flutter/covert_connect_ui/lib/src/status/widgets/state_toggle.dart
+++ flutter/covert_connect_ui/lib/src/status/widgets/state_toggle.dart
@@ -1,4 +1,5 @@
 import 'package:animated_toggle_switch/animated_toggle_switch.dart';
+import 'package:collection/collection.dart';
 import 'package:covert_connect/di.dart';
 import 'package:covert_connect/src/rust/api/service.dart';
 import 'package:covert_connect/src/services/proxy_service.dart';
@@ -24,6 +25,10 @@ class _StateToggleState extends State<StateToggle> {
     if (mounted) setState(() {});
   }
 
+  ProxyState _toProxyState(String value) {
+    return ProxyState.values.firstWhereOrNull((x) => x.name == value) ?? ProxyState.off;
+  }
+
   @override
   void initState() {
     _update();
@@ -45,10 +50,15 @@ class _StateToggleState extends State<StateToggle> {
         border: Border.all(color: dividerColor, width: 1),
         color: colorScheme.surface,
       ),
-      child: AnimatedToggleSwitch<ProxyState>.size(
+      child: AnimatedToggleSwitch<String>.size(
         active: _state != null,
-        current: _state ?? ProxyState.off,
-        values: [ProxyState.off, ProxyState.pac, ProxyState.all],
+        current: switch (_state) {
+          ProxyState.off => ProxyState.off.name,
+          ProxyState.pac => ProxyState.pac.name,
+          ProxyState.all => ProxyState.all.name,
+          _ => "",
+        },
+        values: [ProxyState.off.name, ProxyState.pac.name, ProxyState.all.name],
         borderWidth: 0,
         spacing: 2,
         iconOpacity: 0.67,
@@ -59,12 +69,17 @@ class _StateToggleState extends State<StateToggle> {
         iconAnimationType: AnimationType.onHover,
         styleAnimationType: AnimationType.onHover,
         style: ToggleStyle(borderColor: Colors.transparent, borderRadius: borderRadius),
+        allowUnlistedValues: true,
+        indicatorAppearingDuration: Durations.short4,
+        indicatorAppearingBuilder: (BuildContext context, double value, Widget indicator) {
+          return Opacity(opacity: value.clamp(0.0, 1.0), child: indicator);
+        },
         customSeparatorBuilder: (context, local, global) {
           final opacity = dividerColor.a * ((global.position - local.position).abs() - 0.5).clamp(0.0, 1.0);
           return VerticalDivider(indent: 10.0, endIndent: 10.0, color: dividerColor.withValues(alpha: opacity));
         },
         customIconBuilder: (context, local, global) {
-          final name = switch (local.value) {
+          final name = switch (_toProxyState(local.value)) {
             ProxyState.off => 'OFF',
             ProxyState.pac => 'SMART',
             ProxyState.all => 'ALL',
@@ -83,7 +98,7 @@ class _StateToggleState extends State<StateToggle> {
             ),
           );
         },
-        onChanged: (state) => _setProxyState(state),
+        onChanged: (state) => _setProxyState(_toProxyState(state)),
       ),
     );
   }
