256c461ff2f552a6d796edc85e01e41dd38e2f0e
WickedCode <wickedcode@mail.ru>
2026-01-29 01:29:47 +0300

    add process name by pid implementation for linux

--- crates/sys-connections/src/linux.rs
+++ crates/sys-connections/src/linux.rs
@@ -1,5 +1,17 @@
-use anyhow::{anyhow, Result};
+use anyhow::Result;
+use std::fs;
 
 pub fn get_name_by_pid(pid: u32) -> Result<String> {
-    Err(anyhow!("Not implemented"));
+    let exe_path = format!("/proc/{}/exe", pid);
+
+    match fs::read_link(&exe_path) {
+        Ok(path) => {
+            return Ok(path.display().to_string());
+        }
+        Err(_) => {
+            let comm_path = format!("/proc/{}/comm", pid);
+            let comm = fs::read_to_string(comm_path)?;
+            Ok(comm.trim_end().to_string())
+        }
+    }
 }
