1b6cc79a4717327336ec893912269d49ca932ef9
WickedCode <WickedCode@mail.ru>
2026-01-28 23:21:21 +0300

    use netstat2

--- crates/sys-connections/src/lib.rs
+++ crates/sys-connections/src/lib.rs
@@ -11,3 +11,57 @@ pub use linux::*;
 pub use macos::*;
 #[cfg(target_os = "windows")]
 pub use windows::*;
+
+use anyhow::{Result, bail};
+use netstat2::{get_sockets_info, AddressFamilyFlags, ProtocolFlags, ProtocolSocketInfo};
+use std::net::SocketAddr;
+
+/// The network protocol used by a socket.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum Protocol {
+    /// Transmission Control Protocol.
+    TCP,
+    /// User Datagram Protocol.
+    UDP,
+}
+
+pub fn get_process_name_by_local_addr(addr: SocketAddr, protocol: Protocol) -> Result<String> {
+    let af_flags = if addr.is_ipv6() {
+        AddressFamilyFlags::IPV6
+    } else {
+        AddressFamilyFlags::IPV4
+    };
+    let proto_flags = match protocol {
+        Protocol::TCP => ProtocolFlags::TCP,
+        Protocol::UDP => ProtocolFlags::UDP,
+    };
+    let sockets = get_sockets_info(af_flags, proto_flags)?;
+
+    for si in sockets {
+        match si.protocol_socket_info {
+            ProtocolSocketInfo::Tcp(tcp_si) => {
+                if tcp_si.local_addr == addr.ip() && tcp_si.local_port == addr.port() {
+                    return get_name_by_pids(si.associated_pids, addr);
+                }
+            }
+            ProtocolSocketInfo::Udp(udp_si) => {
+                if udp_si.local_addr == addr.ip() && udp_si.local_port == addr.port() {
+                    return get_name_by_pids(si.associated_pids, addr);
+                }
+            }
+        }
+    }
+
+    bail!("no sockets found");
+}
+
+fn get_name_by_pids(pids: Vec<u32>, addr: SocketAddr) -> Result<String> {
+    if pids.len() > 1 {
+        tracing::warn!("socket with multiple pids: {:?}, socket: {:?}", pids, addr);
+    }
+    if let Some(pid) = pids.first() {
+        get_name_by_pid(*pid)
+    } else {
+        bail!("no pid associated with socket: {:?}", addr);
+    }
+}
---
+++ crates/sys-connections/src/macos.rs
@@ -0,0 +1,4 @@
+use anyhow::{anyhow, Result};
+pub fn get_name_by_pid(pid: u32) -> Result<String> {
+    Err(anyhow!("Not implemented"));
+}
