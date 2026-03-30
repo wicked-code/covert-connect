9e6d0c51efeb852f7266901287db5c17c9445374
WickedCode <WickedCode@mail.ru>
2026-03-30 15:23:05 +0300

    better udp_transfer

--- crates/cc-server/src/udp.rs
+++ crates/cc-server/src/udp.rs
@@ -1,40 +1,46 @@
 
+use std::sync::Arc;
+
 use anyhow::Result;
 use tokio::{
-    io::{AsyncReadExt, AsyncWriteExt, AsyncRead},
+    io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt},
     net::UdpSocket,
 };
 
 // TODO: ??? move MAX_PACKET_SIZE to common place (used many times)
 pub const MAX_PACKET_SIZE: usize = 0xFFFF; // max IP packet size
 
-pub async fn udp_transfer(mut client: impl AsyncWriteExt + AsyncRead + Unpin, out_socket: UdpSocket) -> Result<()> {
-    let mut read_packet_len = 0;
-    let mut read_buf = vec![0u8; MAX_PACKET_SIZE];
-    let mut recv_buf = vec![0u8; MAX_PACKET_SIZE];
-    loop {
-        tokio::select! {
-            result = if read_packet_len == 0 { client.read_exact(&mut read_buf[..2]) } else { client.read_exact(&mut read_buf[..read_packet_len]) } => {
-                result?;
-                if read_packet_len == 0 {
-                    read_packet_len = ((read_buf[0] as usize) << 8) + read_buf[1] as usize;
-                    if read_packet_len > MAX_PACKET_SIZE {
-                        anyhow::bail!("packet size too big");
-                    }
-                } else {
-                    out_socket.send(&read_buf[..read_packet_len]).await?;
-                    read_packet_len = 0;
-                }
-            }
-            result = out_socket.recv(&mut recv_buf[2..]) => {
-                let n = result?;
-                if n == 0 { break; }
-                recv_buf[0] = ((n >> 8) & 0xff) as u8;
-                recv_buf[1] = (n & 0xff) as u8;
-                client.write_all(&recv_buf[..n + 2]).await?;
+pub async fn udp_transfer(client: impl AsyncRead + AsyncWrite + Unpin, out_socket: UdpSocket) -> Result<()> {
+    let (mut reader, mut writer) = tokio::io::split(client);
+    let out_socket = Arc::new(out_socket);
+
+    let client_to_udp = {
+        let socket = out_socket.clone();
+        async move {
+            let mut buf = vec![0u8; MAX_PACKET_SIZE];
+            loop {
+                reader.read_exact(&mut buf[..2]).await?;
+                let len = ((buf[0] as usize) << 8) + buf[1] as usize;
+                reader.read_exact(&mut buf[..len]).await?;
+                socket.send(&buf[..len]).await?;
             }
         }
-    }
+    };
 
-    Ok(())
+    let udp_to_client = async move {
+        let mut buf = vec![0u8; MAX_PACKET_SIZE + 2];
+        loop {
+            let n = out_socket.recv(&mut buf[2..]).await?;
+            if n == 0 { break; }
+            buf[0] = ((n >> 8) & 0xff) as u8;
+            buf[1] = (n & 0xff) as u8;
+            writer.write_all(&buf[..n + 2]).await?;
+        }
+        Ok::<(), anyhow::Error>(())
+    };
+
+    tokio::select! {
+        result = client_to_udp => result,
+        result = udp_to_client => result,
+    }
 }
