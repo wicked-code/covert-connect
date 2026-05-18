33edcdb1f17a9485a91ab483ed587f31badff866
WickedCode <WickedCode@mail.ru>
2026-05-18 19:45:21 +0300

    support long path on windows

--- crates/sys-process/src/windows.rs
+++ crates/sys-process/src/windows.rs
@@ -25,20 +25,21 @@ pub fn path_by_pid(pid: u32) -> Result<String> {
         let process = _guard.0;
 
         const DEFAULT_BUFFER: u32 = MAX_PATH + 1;
+        const LONG_PATH_BUFFER: u32 = 32769;
         let mut buffer_size: u32 = DEFAULT_BUFFER;
         let mut path_buffer = [0u16; DEFAULT_BUFFER as usize];
 
-        QueryFullProcessImageNameW(
+        let result = QueryFullProcessImageNameW(
             process,
             PROCESS_NAME_WIN32,
             windows::core::PWSTR(path_buffer.as_mut_ptr()),
             &mut buffer_size,
-        )?;
+        );
 
-        Ok(if buffer_size < DEFAULT_BUFFER {
+        Ok(if result.is_ok() && buffer_size < DEFAULT_BUFFER {
             String::from_utf16(&path_buffer[0..buffer_size as usize])?
         } else {
-            buffer_size = 32769;
+            buffer_size = LONG_PATH_BUFFER;
             let mut path_buffer = vec![0u16; buffer_size as usize];
             QueryFullProcessImageNameW(
                 process,
